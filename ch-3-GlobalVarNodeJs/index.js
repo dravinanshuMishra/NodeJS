1e496dcf07fa9e656624edfcbcd45aa1356131a8
dravinanshuMishra <dravinanshu97@gmail.com>
2025-08-06 18:11:18 +0530

    local and global variable in nodeJS

---
+++ ch-3-GlobalVarNodeJs/index.js
@@ -0,0 +1,6 @@
+const num = 60
+
+console.log(num); // ye local variable me rahega run and debug karne ke baad.
+
+global.num2 = 67;
+console.log(num2); // ye global variable me jayega run and debug run karne ke baad.

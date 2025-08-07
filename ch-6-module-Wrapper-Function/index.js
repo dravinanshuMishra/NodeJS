122540b874dc3bcaa8168aafc6e75793e906f84a
dravinanshuMishra <dravinanshu97@gmail.com>
2025-08-07 14:27:26 +0530

    module wrapper function

---
+++ ch-6-module-Wrapper-Function/index.js
@@ -0,0 +1,5 @@
+// THIS IS MODULE 6 - Wrapper Function
+(function(exports, require, module, __filename, __dirname){
+//IIEFF function.
+console.log("module 6 - Wrapper Function");
+}(exports, require, module, __filename, __dirname));
